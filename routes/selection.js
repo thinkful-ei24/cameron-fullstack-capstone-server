76aa695f5e471e08ce90184ae89c05bdfd4f9049
Cameron Hatch <cameronlavigne92@gmail.com>
2018-10-15 09:27:09 -0500

    added get endpoint to get list of contestants

---
+++ routes/selection.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const router = express.Router();
+
+const contestants = [
+  'Alex',
+  'Blake',
+  'Chase',
+  'Chris',
+  'Christian',
+  'Christon',
+  'Clay',
+  'Colton',
+  'Connor',
+  'Darius',
+  'David',
+  'Garrett',
+  'Grant',
+  'Jake',
+  'Jason', 
+  'Jean Blanc',
+  'Joe',
+  'John',
+  'Jordan',
+  'Kamil',
+  'Leo',
+  'Lincoln',
+  'Mike',
+  'Nick',
+  'Rickey',
+  'Ryan',
+  'Trent',
+  'Wills'
+];
+
+router.get('/', (req, res, next) => {
+  res.json(contestants);
+});
